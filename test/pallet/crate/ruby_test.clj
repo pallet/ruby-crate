6fa0ee819ef444c3e30b0a4207bc4c64789cd646
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2013-02-12 22:02:33 -0500

    port to 0.8.x architecture

--- test/pallet/crate/ruby_test.clj
+++ test/pallet/crate/ruby_test.clj
@@ -1,15 +1,15 @@
 (ns pallet.crate.ruby-test
   (:use pallet.crate.ruby
         clojure.test
-        pallet.test-utils)
+        pallet.test-utils
+        [pallet.actions :only [package packages package-manager exec-script exec-checked-script]])
   (:require
-   [pallet.build-actions :as build-actions]
-   [pallet.action.exec-script :as exec-script]))
+   [pallet.build-actions :as build-actions]))
 
 (deftest invoke-test
   (is (build-actions/build-actions
        {}
        (ruby)
-       (exec-script/exec-script
+       (exec-script
         @(ruby-version))
        (ruby-packages))))
